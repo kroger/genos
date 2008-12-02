beb89e4ef781f58f8ab8dc8a43abdf9b23242cf0
Alexandre Passos <alexandre.tp@gmail.com>
2008-12-02 19:24:00 -0200

    Refatorando latex.mk
    Agora as coisas têm o nome que deveriam. Pra incluir, por exemplo,
    svgs gerados por lilypond, se adiciona LILY_SVG no OTHER. Para colocar
    pdf a partir do gnuplot, GNUPLOT_PDF no OTHER, etc.
    
    Também as regras do clean estão mais limpas.

--- make/latex.mk
+++ make/latex.mk
@@ -2,37 +2,38 @@ LILYDIR = lily
 FIGSDIR = figs
 DATADIR = data
 
-OTHER_PNG1= $(patsubst %.dia,%.png,$(wildcard $(FIGSDIR)/*.dia))
-OTHER_EPS1= $(patsubst %.dia,%.eps,$(wildcard $(FIGSDIR)/*.dia))
-OTHER_EPS2= $(patsubst %.svg,%.eps,$(wildcard $(FIGSDIR)/*.svg))
-OTHER_EPS3= $(patsubst %.plot,%.eps,$(wildcard $(DATADIR)/*.plot))
-OTHER_EPS4= $(patsubst %.png,%.eps,$(wildcard $(FIGSDIR)/*.png))
+DIA_PNG= $(patsubst %.dia,%.png,$(wildcard $(FIGSDIR)/*.dia))
+DIA_EPS= $(patsubst %.dia,%.eps,$(wildcard $(FIGSDIR)/*.dia))
+CLEAN_FILES+= $(DIA_EPS:.dia=.eps)
+CLEAN_FILES+= $(DIA_PNG:.dia=.png)
 
-OTHER_PDF3= $(patsubst %.plot,%.pdf,$(wildcard $(DATADIR)/*.plot))
-OTHER_PDF4= $(patsubst %.png,%.pdf,$(wildcard $(FIGSDIR)/*.png))
+PNG_EPS= $(patsubst %.png,%.eps,$(wildcard $(FIGSDIR)/*.png))
+PNG_PDF= $(patsubst %.png,%.pdf,$(wildcard $(FIGSDIR)/*.png))
+
+GNUPLOT_PDF= $(patsubst %.plot,%.pdf,$(wildcard $(DATADIR)/*.plot))
+GNUPLOT_EPS= $(patsubst %.plot,%.eps,$(wildcard $(DATADIR)/*.plot))
+CLEAN_FILES+= $(GNUPLOT_PDF:.plot=.pdf)
+CLEAN_FILES+= $(GNUPLOT_EPS:.plot=.eps)
 
 LILY_EPS= $(patsubst %.ly,%.eps,$(wildcard $(LILYDIR)/*.ly))
 LILY_PNG= $(patsubst %.ly,%.png,$(wildcard $(LILYDIR)/*.ly))
 LILY_PS = $(patsubst %.ly,%.ps,$(wildcard $(LILYDIR)/*.ly))
 LILY_PDF= $(patsubst %.ly,%.pdf,$(wildcard $(LILYDIR)/*.ly))
 LILY_SVG= $(patsubst %.ly,%.svg,$(wildcard $(LILYDIR)/*.ly))
-OTHER_PNG2= $(patsubst %.svg,%.png,$(wildcard $(FIGSDIR)/*.svg))
-
-INKSCAPE_PDF = $(patsubst %.svg,%.pdf,$(wildcard $(FIGSDIR)/*.svg))
-
-#OTHER += $(LILY_EPS) $(LILY_PNG) $(LILY_PS) $(LILY_PDF) $(LILY_SVG) $(OTHER_PNG2) 
-
-OTHER += $(OTHER_EPS1) $(OTHER_EPS2) $(OTHER_EPS3) $(OTHER_PNG1) 
-
-CLEAN_FILES+= $(OTHER_EPS3:.plot=.eps)
-CLEAN_FILES+= $(OTHER_EPS2:.svg=.eps)
-CLEAN_FILES+= $(OTHER_EPS1:.dia=.eps)
-CLEAN_FILES+= $(OTHER_PNG1:.dia=.png)
 CLEAN_FILES+= $(LILY_EPS:.ly=.eps)
 CLEAN_FILES+= $(LILY_PNG:.ly=.png)
 CLEAN_FILES+= $(LILY_PS:.ly=.ps)
 CLEAN_FILES+= $(LILY_PDF:.ly=.pdf)
-CLEAN_FILES+= $(LILY_SVG:.ly=.svg)
+
+TXT_HTML= $(patsubst %.txt,%.html,$(wildcard *.txt))
+CLEAN_FILES+= $(TXT_HTML:.txt=.html)
+
+SVG_PNG= $(patsubst %.svg,%.png,$(wildcard $(FIGSDIR)/*.svg))
+SVG_PDF = $(patsubst %.svg,%.pdf,$(wildcard $(FIGSDIR)/*.svg))
+SVG_EPS= $(patsubst %.svg,%.eps,$(wildcard $(FIGSDIR)/*.svg))
+CLEAN_FILES+= $(SVG_PNG:.svg=.pdf)
+CLEAN_FILES+= $(SVG_PDF:.svg=.png)
+CLEAN_FILES+= $(SVG_EPS:.svg=.eps)
 
 CLEAN_BEAMER = .nav .snm .vrb
 CLEAN_FILES+= $(foreach suffix,$(CLEAN_BEAMER),$(addsuffix $(suffix),$(NAME)))
@@ -44,6 +45,7 @@ LATEX_ENV+= TEXINPUTS=~/lib/latex//:~/lib/emacs/bbdb/tex/:~/lib/license//:src:co
 vpath %.eps $(FIGSDIR)
 vpath %.eps $(DATADIR)
 vpath %.ly $(LILYDIR)
+vpath %.svg $(FIGSDIR)
 
 #BIBTEXSRCS = 
 #USE_HEVEA = 1
@@ -51,6 +53,7 @@ vpath %.ly $(LILYDIR)
 #USE_PDFLATEX = 1
 #USE_TEX2PAGE = 1
 #USE_DVIPDFM = 1
+#VIEWPDF
 
 doc: pdf
 
